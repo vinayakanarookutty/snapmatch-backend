a8d0bdd1edb5622bb93862a6c80111a2c3006932
Vinayak S <vinayaksukhalal@gmail.com>
2024-02-12 01:01:32 +0530

    first commit

--- index.js
+++ index.js
@@ -5,21 +5,8 @@ var bodyparser=require('body-parser')
 app.use(bodyparser.urlencoded({extended:true}))
 var home=require('./home')
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
+const path = require('path'); 
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-  }));  
-// app.use(
-//     '/firebase-api',
-//     createProxyMiddleware({
-//       target: 'https://firebasestorage.googleapis.com',
-//       changeOrigin: true,
-//       pathRewrite: {
-//         '^/firebase-api': '', // remove the '/firebase-api' prefix when forwarding
-//       },
-//     })
-//   );
   app.use(express.static('public'));
   app.use(express.static('models'));
   // Serve models from the 'models' directory
