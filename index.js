4fc4195775903f4c09ef0bd3d1c9ff61dfa26a0a
Vinayak S <103236060+vinayakanarookutty@users.noreply.github.com>
2025-06-20 16:34:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ var bodyparser = require('body-parser')
 
 // CORS configuration MUST come BEFORE other middleware
 const allowedOrigin = 'https://d1mmkc91smmqma.cloudfront.net';
-app.use(cors({
-  origin: allowedOrigin,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // enable if you use cookies or auth headers
-}));
+app.use(cors());
 
 // Body parser middleware
 app.use(bodyparser.urlencoded({extended: true, limit: '10mb'}))
